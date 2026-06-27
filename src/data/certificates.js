1e55a6f05fe8a849d1c561cd79938fe4d9df03c5
elelcahyani <dwcelfa@domain.com>
2026-06-27 22:11:33 +0700

    feat: tambahkan sertifikat baru dan perbarui data sertifikat

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -3,41 +3,48 @@
 export const certificates = [
   {
     id: 1,
-    Img: "/uploads/certificates/certif1.png",
-    title: "React Developer Certification",
-    issuer: "Meta (Facebook)",
-    date: "2024"
+    Img: "/uploads/certificates/certif7.jpeg",
+    title: "Intensive Internship program - Software Engineer",
+    issuer: "PT Arus Digital Sinergi",
+    date: "Jan - March 2026"
   },
   {
     id: 2,
-    Img: "/uploads/certificates/certif2.png",
-    title: "JavaScript Algorithms and Data Structures",
-    issuer: "freeCodeCamp",
-    date: "2023"
-  },
-  {
-    id: 3,
-    Img: "/uploads/certificates/certif3.jpg",
-    title: "Web Development Certification",
-    issuer: "Udemy",
+    Img: "/uploads/certificates/certif1.png",
+    title: "React Developer Certification",
+    issuer: "Rakamin Academy",
     date: "2024"
   },
   {
+    id: 3,
+    Img: "/uploads/certificates/certif2.png",
+    title: "JavaScript Algorithms and Data Structures",
+    issuer: "Rakamin Academy",
+    date: "2023"
+  },
+  {
     id: 4,
-    Img: "/uploads/certificates/certif4.png",
-    title: "Full Stack Development",
-    issuer: "Coursera",
+    Img: "/uploads/certificates/certif3.jpg",
+    title: "Web Development Certification",
+    issuer: "Brighten English Academy",
     date: "2024"
   },
   {
     id: 5,
-    Img: "/uploads/certificates/certif5.jpeg",
-    title: "Data Science Certification",
-    issuer: "DataCamp",
+    Img: "/uploads/certificates/certif4.png",
+    title: "Full Stack Development",
+    issuer: "Komdigi",
     date: "2024"
   },
   {
     id: 6,
+    Img: "/uploads/certificates/certif5.jpeg",
+    title: "Data Science Certification",
+    issuer: "",
+    date: "2024"
+  },
+  {
+    id: 7,
     Img: "/uploads/certificates/certif6.jpg",
     title: "AI & Machine Learning",
     issuer: "Google",
